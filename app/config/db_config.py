c395dbc8ecfe6ccff5fd4efc8e0f3e4cbb739104
CarlosHdez82o <cahernan82@hotmail.com>
2026-04-02 06:57:21 -0500

    Otra  Actualización

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,11 +1,14 @@
 import psycopg2
+import socket
 
 def get_db_connection():
+    host = "db.irwkwwtrimebxuaqvglf.supabase.co"
+    addr = socket.gethostbyname(host)  # fuerza IPv4
     return psycopg2.connect(
-        host="db.irwkwwtrimebxuaqvglf.supabase.co",
+        host=addr,
         port="5432",
         user="postgres",
         password="chclmdlvg821130",
         dbname="postgres",
-        sslmode="require"  # <--- ESTA LÍNEA ES OBLIGATORIA PARA RENDER
+        sslmode="require"
     )
