366d5ec3629b2f8242ddcdaee2c823387934ef1b
CarlosHdez82o <cahernan82@hotmail.com>
2026-04-02 07:13:10 -0500

    Otra mas Actualización

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,14 +1,15 @@
+import os
 import psycopg2
 import socket
 
 def get_db_connection():
-    host = "db.irwkwwtrimebxuaqvglf.supabase.co"
+    host = os.getenv("DB_HOST", "db.irwkwwtrimebxuaqvglf.supabase.co")
     addr = socket.gethostbyname(host)  # fuerza IPv4
     return psycopg2.connect(
         host=addr,
-        port="5432",
-        user="postgres",
-        password="chclmdlvg821130",
-        dbname="postgres",
-        sslmode="require"
+        port=os.getenv("DB_PORT", "5432"),
+        user=os.getenv("DB_USER", "postgres"),
+        password=os.getenv("DB_PASSWORD", "TU_PASSWORD"),
+        dbname=os.getenv("DB_NAME", "postgres"),
+        sslmode=os.getenv("DB_SSLMODE", "require")
     )
