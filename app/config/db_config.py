dfd5902b8d6df200d3ec60b3245a0c0ae48438ca
CarlosHdez82o <cahernan82@hotmail.com>
2026-04-02 03:41:34 -0500

    Versión inicial estable de la API

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def get_db_connection():
     return psycopg2.connect(
-        host="db.irwkwwtrimebxuaqvglf.supabase.co",
+        host="db.irwkwwtrimebxuaqvglf.supabase.co"
         port="5432",
         user="postgres",
         password="chclmdlvg821130",
