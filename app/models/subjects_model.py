d545b966473dfac8ba483cf671ee62b8196f683a
CarlosHdez82o <cahernan82@hotmail.com>
2026-05-10 21:47:45 -0500

    Fix: Se Ajusto el modelo de materias

--- app/models/subjects_model.py
+++ app/models/subjects_model.py
@@ -3,16 +3,17 @@ from typing import Optional
 from datetime import datetime
 
 class SubjectBase(BaseModel):
-    code: str
     name: str
     credits: int
     program_id: int
+    code: Optional[str] = None
 
 class SubjectCreate(SubjectBase):
     pass
 
 class SubjectResponse(SubjectBase):
     id: int
+    program_name: Optional[str] = None
     created_at: Optional[datetime] = None
     updated_at: Optional[datetime] = None
 
